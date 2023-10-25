45622ac50fb94d24b18e3a4fd9894ed8f386bea6
ananyajain827 <134041171+ananyajain827@users.noreply.github.com>
2023-10-25 23:12:45 +0530

    Add files via upload

---
+++ PIZZAC.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for n in range(t):
+    x=int(input())
+    if(x%2==0):
+        print("yes")
+    elif(x==1):
+        print("yes")
+    else:
+        print("no")
